0ffbe665fedddcdda140a14d0714a061f8d916a8
smmadrigal <145819473+smmadrigal@users.noreply.github.com>
2023-09-23 10:32:33 +0800

    Add files via upload

---
+++ mPortfolio/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".single-text", {
+    strings: ["Galendez"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".social-text", {
+    strings: ["Socials"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
